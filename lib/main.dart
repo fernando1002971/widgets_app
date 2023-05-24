824ecbc38a2ef72a88be319939b884aa3138bce0
fernando1002971 <fernando1002971@gmail.com>
2023-05-24 00:13:41 -0600

    Primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Chip(label: Text("fdasdasd")),
+          child:  Text("fdasdasd"),
         ),
       ),
     );
