2d7fc6abfb63d29595a728b5b6c8cfc66d913407
fernando1002971 <fernando1002971@gmail.com>
2023-05-27 19:55:20 -0600

    Botón personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -28,87 +28,94 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-final colors = Theme.of(context).colorScheme;
+    final colors = Theme.of(context).colorScheme;
 
     return SizedBox(
       width: double.infinity,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Wrap(
-        spacing: 10,
-         alignment: WrapAlignment.center,
-         children: [
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Wrap(
+            spacing: 10,
+            alignment: WrapAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {}, child: const Text("Elevated Button")),
 
-          ElevatedButton(
-            onPressed: (){},
-             child: const Text("Elevated Button")
-             ),
-    
-          const ElevatedButton(
-            onPressed: null,
-             child: Text("Elevated Button disabled")
-             ),
-    
-             ElevatedButton.icon(
-             onPressed: (){}, 
-             icon: const Icon(Icons.access_alarm_rounded), 
-             label: const Text("Elevated Icon")
-             ),
+              const ElevatedButton(
+                  onPressed: null, child: Text("Elevated Button disabled")),
 
-             FilledButton(
-             onPressed: (){}, 
-             child: const Text("Filled"),
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.access_alarm_rounded),
+                  label: const Text("Elevated Icon")),
 
-             ),
-
-             FilledButton.icon(
-              onPressed: (){}, 
-              icon: const Icon(Icons.access_time_sharp), 
-              label: const Text("Filled Icon")
+              FilledButton(
+                onPressed: () {},
+                child: const Text("Filled"),
               ),
+
+              FilledButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.access_time_sharp),
+                  label: const Text("Filled Icon")),
 
               OutlinedButton(
-                onPressed: (){}, 
-                child: const Text("Outline button")
-                ),
+                  onPressed: () {}, child: const Text("Outline button")),
 
-             OutlinedButton.icon(
-              onPressed: (){}, 
-               icon: const Icon(Icons.wysiwyg),
-               label: const Text("Outline Icon"),
-              ),
-    
-             TextButton(
-               onPressed: (){}, 
-               child: const Text("Text")
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.wysiwyg),
+                label: const Text("Outline Icon"),
               ),
 
-             TextButton.icon(
-               onPressed: (){}, 
-               icon: const Icon(Icons.add_business),
-               label: const Text("Text Icon")
-              ),
+              TextButton(onPressed: () {}, child: const Text("Text")),
 
-              //TODO: CUSTOM BUTTOM
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_business),
+                  label: const Text("Text Icon")),
+
+              const CustomButton(),
+
+
 
 
               IconButton(
-                onPressed: (){},
-                 icon: const Icon(Icons.face_4_rounded)
-              ),
+                  onPressed: () {}, icon: const Icon(Icons.face_4_rounded)),
 
               IconButton(
-                onPressed: (){},
-                 icon: const Icon(Icons.face_4_rounded),
-                 style: ButtonStyle(
+                onPressed: () {},
+                icon: const Icon(Icons.face_4_rounded),
+                style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(colors.primary),
                   iconColor: const MaterialStatePropertyAll(Colors.white),
-                 ),
+                ),
               ),
+            ],
+          )),
+    );
+  }
+}
 
-         ],
-        )
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Text("Custom Button", style: TextStyle(color:Colors.white))
+            ),
+        ),
       ),
     );
   }
